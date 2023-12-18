7a1d065e1112d37330c0716562085ef278de508e
Kamal Marouane <kamal.marouane@grenoble-inp.org>
2023-12-18 06:07:21 +0100

    Reduce execution time for file sending and calculating file sending execution time

--- src/network/master/Master.java
+++ src/network/master/Master.java
@@ -2,10 +2,13 @@ package network.master;
 
 import network.node.PingPongInterface;
 import parser.*;
+import performance.FileTransferLogger;
 import scheduler.LocalScheduler;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.Naming;
 import java.util.List;
 
@@ -14,18 +17,23 @@ import static hosts.RetrieveHosts.getMasterName;
 
 public class Master {
 
+    public static FileTransferLogger logger = new FileTransferLogger();
+
     private static int executeACommand(String command,String host,String master, Node node){
         int exitCode = -1;
         try {
+            PingPongInterface exec = (PingPongInterface) Naming.lookup("rmi://"+host+":3000/PingPongObject");
             List<Node> deps = LocalScheduler.graph.get(node);
             if(deps !=null){
                 for (Node dep : deps) {
-                    if(dep.getIsFile()) sendFile(master,host,"./"+dep.getNodeName());
-                    else if(hasFileExtension(dep.getNodeName())) sendFile(master,host,dep.getNodeName());
-                    else System.out.println(dep.getNodeName());
+                    int exists = exec.executeCommand("ls " + dep.getNodeName());
+                    if(exists!=0){
+                        if(dep.getIsFile()) sendFile(master,host,"./"+dep.getNodeName());
+                        else if(hasFileExtension(dep.getNodeName())) sendFile(master,host,dep.getNodeName());
+                    }
+                    else System.out.println("*-*-*-*-*-*-*-*-*-* File " + dep.getNodeName() + " Already sent to " + host);
                 }
             }
-            PingPongInterface exec = (PingPongInterface) Naming.lookup("rmi://"+host+":3000/PingPongObject");
             exitCode = exec.executeCommand(command);
 
             // Print the result
@@ -79,6 +87,10 @@ public class Master {
 //            System.out.println("Sending " + source + " to " + destination + " using file "+target);
 //            String source = "petitprince-9.luxembourg.grid5000.fr:try.txt";
 //            String destination = "petitprince-11.luxembourg.grid5000.fr";
+        long startTime0 = System.nanoTime();
+        long endTime0 = System.nanoTime();
+        long latency0 = endTime0 - startTime0;
+        long fileSendingStartTime = System.nanoTime();
         String command = "scp " + source + ":" + target + " " + destination + ":~";
         System.out.println("+++++++++ "+command);
 
@@ -93,10 +105,44 @@ public class Master {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
+        long fileSendingEndTime = System.nanoTime();
+        long fileSendingDuration = (fileSendingEndTime - fileSendingStartTime - latency0) / 1_000_000;
+        System.out.println("File Sending Execution time : " + fileSendingDuration + " ms  for file : " +target);
+        logger.addRecord(target,destination,fileSendingDuration);
     }
     public static boolean hasFileExtension(String fileName) {
         return fileName.contains(".") && fileName.lastIndexOf('.') > 0;
+    }
+
+    public static boolean canSend(String filePath) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        // Command to find the file
+        processBuilder.command("bash", "-c", "find . -name " + filePath);
+
+        try {
+            Process process = processBuilder.start();
+
+            // Reading the output of the command
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            boolean fileFound = false;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(filePath)) {
+                    System.out.println("/////////////////////////////// :"+line);
+                    fileFound = true;
+                    break;
+                }
+            }
+            // Wait to get exit value
+            int exitCode = process.waitFor();
+            System.out.println("Exit value is : " + exitCode);
+
+            return !fileFound;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static int master(String[] args,String masterName, Node node) {
