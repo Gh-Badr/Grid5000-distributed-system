4b7096358b2910791144c5c5540cdc0c13930cf7
Gh-Badr <ghazaoui.badr.tech@gmail.com>
2023-12-20 00:20:54 +0100

    Correct model simulation

--- test/model/lol.py
+++ test/model/lol.py
@@ -6,7 +6,7 @@ MB_per_second_to_GB_per_second = 1 / 125  # 1 MB per second in GB per second
 
 
 def Y(n):
-    return 5*(N//n+1) + 20*MB_per_second_to_GB_per_second + 20*2*0.06 + 10
+    return 5*(N//n+1) + 20*MB_per_second_to_GB_per_second/1024+ (60-20%n)*MB_per_second_to_GB_per_second/1024  + (n+60-20%n)*2*0.06 + 10
 
 # Generate n values
 n_values = np.linspace(0,30,30)  # Adjust the range and number of points as needed
@@ -15,7 +15,7 @@ n_values = np.linspace(0,30,30)  # Adjust the range and number of points as need
 Y_values = Y(n_values)
 
 # Plot the graph
-plt.plot(n_values, Y_values, label='Y = 5*(20//n) + 20*1MB/(125MB/s) + 20*2*latency + C')
+plt.plot(n_values, Y_values)
 plt.xlabel('n')
 plt.ylabel('Y')
 plt.title('Execution time Y per number of nodes reserved n')
